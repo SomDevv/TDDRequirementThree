616a5046aeeb4bae1bcf17bd7b53a5dc933e1093
nik-052 <ns5648@srmist.edu.in>
2021-10-12 14:58:02 +0530

    Second commit

--- src/Base_class/main_base.java
+++ src/Base_class/main_base.java
@@ -4,5 +4,6 @@ public class main_base {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Second hello world");
+        System.out.println("Third hello world");
     }
 }
