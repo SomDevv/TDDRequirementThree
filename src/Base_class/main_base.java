05ef9773b53f56c93fb1e9b2a38acc67cb457cb5
nik-052 <ns5648@srmist.edu.in>
2021-10-12 14:57:41 +0530

    First commit

--- src/Base_class/main_base.java
+++ src/Base_class/main_base.java
@@ -3,5 +3,6 @@ package Base_class;
 public class main_base {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Second hello world");
     }
 }
